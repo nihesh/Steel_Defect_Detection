d81eaebb6f3180411bf1a7b438cf740c00fcee6d
nihesh <nihesh16059@iiitd.ac.in>
2019-10-20 11:32:30 +0530

    Added focal loss

--- Train.py
+++ Train.py
@@ -5,13 +5,45 @@ import torch
 import torch.nn as nn
 import numpy as np
 from matplotlib import pyplot as plt
-from Args import DIM, ROOT, EPOCHS, BATCH_SIZE, NUM_WORKERS, LEARNING_RATE, ALPHA, BETA, NUM_CLASSES, SAVE_PATH, LOAD_PATH
+from Args import DIM, ROOT, EPOCHS, BATCH_SIZE, NUM_WORKERS, LEARNING_RATE, ALPHA, BETA, NUM_CLASSES, SAVE_PATH, LOAD_PATH, GAMMA
 from DatasetReader import DatasetReader
 from unet_model import UNet		# Change unet_model to model for my implementation of unet
 import torch.optim as optim
 import torch.functional as F
 from copy import deepcopy
 from Evaluation import MeanDiceCoefficient
+from matplotlib import pyplot as plt
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1,1)
+
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type()!=input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0,target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average: return loss.mean()
+        else: return loss.sum()
 
 def tversky_loss(true, logits, alpha, beta, eps=1e-7):
 
@@ -49,6 +81,7 @@ if(__name__ == "__main__"):
 	
 	loss_fn = nn.CrossEntropyLoss().cuda()
 	BCELoss = torch.nn.BCEWithLogitsLoss()
+	focal = FocalLoss(gamma = GAMMA)
 	optimiser = optim.Adam(model.parameters(), lr = LEARNING_RATE)
 
 	trainset = DatasetReader(ROOT + "train/")
@@ -58,7 +91,14 @@ if(__name__ == "__main__"):
 	trainloader = torch.utils.data.DataLoader(trainset, batch_size = BATCH_SIZE, shuffle = True, num_workers = NUM_WORKERS)
 	testloader = torch.utils.data.DataLoader(testset, batch_size = BATCH_SIZE, shuffle = True, num_workers = NUM_WORKERS)
 
+	epoch_axis = []
+	loss_axis = []
+	train_dice_axis = []
+	test_dice_axis = []
+
 	for epoch in range(EPOCHS):
+
+		epoch_axis.append(epoch + 1)
 
 		# Training phase
 		train_epoch_loss = 0
@@ -76,6 +116,7 @@ if(__name__ == "__main__"):
 			# BCE Loss
 			one_hot_vector = one_hot(target)
 			loss = loss + BCELoss(output, one_hot_vector)
+			loss = loss + focal(output, one_hot_vector)
 
 			# Multi class CE
 			# loss = loss_fn(output, target)
@@ -125,5 +166,20 @@ if(__name__ == "__main__"):
 				test_acc = test_acc / len(testset)
 			))
 
+		loss_axis.append(train_epoch_loss / len(trainset))
+		train_dice_axis.append(train_dice / len(trainset))
+		test_dice_axis.append(test_dice / len(testset))
+
 		torch.save(model, SAVE_PATH + "epoch_" + str(epoch) + "_DICE_" + str(train_dice / len(trainset)) + ".pth")
  
+	plt.clf()
+	plt.plot(epoch_axis, loss_axis)
+	plt.savefig("./Results/loss.jpg")
+
+	plt.clf()
+	plt.plot(epoch_axis, train_dice_axis)
+	plt.savefig("./Results/train_dice.jpg")
+
+	plt.clf()
+	plt.plot(epoch_axis, test_dice_axis)
+	plt.savefig("./Results/test_dice.jpg")
